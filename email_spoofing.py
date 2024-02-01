0584a02cb7208979c57ba7da10315905c004ab7b
Wee Kang Lee <jasonleeweekang@gmail.com>
2024-02-01 19:01:06 +0800

    fixed decryption

--- email_spoofing.py
+++ email_spoofing.py
@@ -13,9 +13,33 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 KDF_ALGORITHM = hashes.SHA256()
 KDF_LENGTH = 32
 KDF_ITERATIONS = 120000
+api_key = ""
+save_exists = False
 credentials_path = "./credentials.txt"
-api_key = "" 
-domain = input("Enter domain to search: ")
+if os.path.exists(credentials_path):
+    save_exists = True
+
+
+def check_url_extension(url):
+    valid_extensions = [".com", ".sg", ".net"]
+
+    # Remove any query parameters or fragments
+    url_without_params = url.split('?')[0].split('#')[0]
+    for extension in valid_extensions:
+        if url_without_params.endswith(extension):
+            return True
+    return False
+
+
+# Get domain name
+while True:
+    domain = input("Enter domain to search: ")
+
+    if check_url_extension(domain):
+        break
+
+    print("Enter a valid domain!")
+
 
 def encrypt(plaintext: str, password: str) -> (bytes, bytes):
     # Derive a symmetric key using the passsword and a fresh random salt.
@@ -31,27 +55,27 @@ def encrypt(plaintext: str, password: str) -> (bytes, bytes):
 
     return ciphertext, salt
 
+
 def decrypt(ciphertext: bytes, password: str, salt: bytes) -> str:
     # Derive the symmetric key using the password and provided salt.
     kdf = PBKDF2HMAC(
         algorithm=KDF_ALGORITHM, length=KDF_LENGTH, salt=salt,
         iterations=KDF_ITERATIONS)
     key = kdf.derive(password.encode("utf-8"))
-
     # Decrypt the message
     f = Fernet(base64.urlsafe_b64encode(key))
-    plaintext = f.decrypt(ciphertext)
 
+    plaintext = f.decrypt(ciphertext)
     return plaintext.decode("utf-8")
-    
-    
+
+
 def check_for_prerequisites():
     # check if jq is downloaded
     result = subprocess.run(['dpkg', '-s', 'jq'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+
     if result.returncode == 1:
         os.system("sudo apt install jq")
-    
+
     # check if smtp-email-spoofer is downloaded
     if not os.path.isdir("./smtp-email-spoofer-py"):
         print("Downloading required repository..")
@@ -66,57 +90,72 @@ def main():
     else:
         retrieve_domain_json(domain)
 
+
 def save_session(username, password, api_key, smtp_server, port):
+    if save_exists:
+        sys.exit()
+
     save_session = input("Do you want to save your session? (Y/N): ")
     allowed_answers = ["Y", "N"]
-    
     if save_session.upper() not in allowed_answers:
         print("Invalid input.. exiting program")
         sys.exit()
-    
-    if save_session.upper() == "Y" :
+
+    if save_session.upper() == "Y":
         session_pass = getpass.getpass("Enter a password for encryption: ")
         encrypted_password, salt_pass = encrypt(password, session_pass)
         encrypted_apikey, salt_api = encrypt(api_key, session_pass)
-        
+
+        api_key = decrypt(encrypted_apikey, session_pass, salt_api)
+
         with open(credentials_path, 'w') as file:
             file.write(username + "\n")
             file.write(encrypted_password.decode() + "\n")
             file.write(encrypted_apikey.decode() + "\n")
             file.write(smtp_server + "\n")
             file.write(port)
-            
+
+        salts = []
         with open('./salts', 'wb') as file:
             file.write(salt_pass)
             file.write(salt_api)
-            
-def read_from_save():
-    
-    session_pass = input("Enter session password: ")
-    file = open(credentials_path)
-    content = file.readlines()
-    
-    username = content[0].rstrip('\n')
-    encrypted_pass = content[1].rstrip('\n')
-    encrypted_apikey = content[2].rstrip('\n')
-    smtp_server = content[3].rstrip('\n')
-    port = content[4]
-    file.close()
-    
-    salts = []
-    file = open('./salts', mode="rb")
-    for i in range(2):
-        salt = file.read(16)
-        salts.append(salt)
-        
-    file.close()
 
-    password = decrypt(str.encode(encrypted_pass), session_pass, salts[0])
-    api_key = decrypt(str.encode(encrypted_apikey), session_pass, salts[1])
-    return username, password, smtp_server, port, api_key
+    print("Saved successfully.")
+
+
+def read_from_save():
+    try:
+        session_pass = getpass.getpass("Enter session password: ")
+        file = open(credentials_path)
+        content = file.readlines()
+
+        username = content[0].rstrip('\n')
+        encrypted_pass = content[1].rstrip('\n')
+        encrypted_apikey = content[2].rstrip('\n')
+        smtp_server = content[3].rstrip('\n')
+        port = content[4]
+        file.close()
+
+        salts = []
+        file = open('./salts', mode="rb")
+        for i in range(2):
+            salt = file.read(16)
+            salts.append(salt)
+
+        file.close()
+
+        password = decrypt(str.encode(encrypted_pass), session_pass, salts[0])
+        api_key = decrypt(str.encode(encrypted_apikey), session_pass, salts[1])
+
+        return username, password, smtp_server, port, api_key
+
+    except:
+        print("Incorrect password")
+        return
+
 
 def spoof(attacker_email):
-    if os.path.isfile(credentials_path):
+    if save_exists:
         username, password, smtp_server, port = read_from_save()[0:4]
     else:
         username = input("Enter SMTP username: ")
@@ -124,90 +163,96 @@ def spoof(attacker_email):
         # smtp-relay.brevo.com
         smtp_server = input("STMP server host: ")
         port = input("Enter port number: ")
-    
-    # SPECIFY VICTIM EMAILS (support@cysecure.co cysecureco@outlook.com)
-    victim_emails = input("Enter victim emails to send to (Seperated by a space):")
-    
+
+    # SPECIFY VICTIM EMAILS (for ex: support@cysecure.co cysecureco@outlook.com)
+    victim_emails = input("Enter victim emails to send to (Seperated by a space): ")
+
     command = f"python3 ./smtp-email-spoofer-py/spoof.py cli --username {username} --password {password} --host {smtp_server} --port {port} --sender {attacker_email} --name {domain} --recipients {victim_emails} --subject 'AES Vulnerability Test' --filename ./smtp-email-spoofer-py/message_body.html"
     os.system(command)
-    
-    print("Email sent successfully. Check your inbox now.")
-    print("Hint: Check the spam folder..")
-    
-    save_session(username, password, api_key, smtp_server, port)
-    print("k")
-    
-def email_spoofer(domain):
 
+    print("Executed.")
+    print("Hint: Check the spam folder..")
+
+    save_session(username, password, api_key, smtp_server, port)
+
+
+def email_spoofer(domain):
     # Verify if github package is installed
     email_list = []
     file_path = f"./email_lists/{domain}_emails.txt"
     with open(file_path, 'r') as file:
         for line in file:
             email_list.append(line.rstrip('\n'))
-            
+
     executing = True
     while executing:
         print("List of emails:")
         x = 1
         for email in email_list:
-    	    print(f"[{x}] {email}")
-    	    x += 1
-    	
+            print(f"[{x}] {email}")
+            x += 1
+
         try:
             chosen_index = int(input("Select email (Enter index): "))
-            
+
         # Input validation 1
         except ValueError:
             print("Number pls")
             continue
-            
-    	# Input validation 2
+
+        # Input validation 2
         if not (0 < int(chosen_index) <= len(email_list)):
             print("Invalid index. Number does not exist")
             continue  # Skip the rest of the loop and start over
         else:
             executing = False
-            
-    chosen_email = email_list[chosen_index-1]
+
+    chosen_email = email_list[chosen_index - 1]
 
     # Craft email body
     if os.path.isfile(f"./email_lists/{domain}_emails.txt"):
-    	os.system("echo 'This is a test for business email compromise vulnerability. If email can be received, it means that the sending domain name is vulnerable towards email impersonation.' > ./smtp-email-spoofer-py/message_body.html")
-    	
-    spoof(chosen_email)
-    
-    
-def retrieve_domain_json(domain):
+        os.system(
+            "echo 'This is a test for business email compromise vulnerability. If email can be received, it means that the sending domain name is vulnerable towards email impersonation.' > ./smtp-email-spoofer-py/message_body.html")
 
-    if os.path.exists(credentials_path):
-        api_key = read_from_save()[4]
+    spoof(chosen_email)
+
+
+def retrieve_domain_json(domain):
+    global api_key
+    if save_exists:
+        print("Detected previous save...")
+        try:
+            api_key = read_from_save()[4]
+        except:
+            api_key = getpass.getpass("Enter Hunter.io API key: ")
+
     else:
         api_key = getpass.getpass("Enter Hunter.io API key: ")
-    
+
     custom_url = "https://api.hunter.io/v2/domain-search?domain=" + domain + "&" + "api_key=" + api_key
-    #GET_command = "curl " + custom_url + f" > {domain}_details.json"
-    
+    # GET_command = "curl " + custom_url + f" > {domain}_details.json"
+
     # write curl output into file
     json_file_path = f"./json_files/{domain}_details.json"
+
     os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
     with open(json_file_path, 'w') as file:
-    	output = subprocess.run(['curl', custom_url], stdout=file)
+        output = subprocess.run(['curl', custom_url], stdout=file)
 
     # If domain is invalid
     if output.returncode == 3:
         print("Domain may be invalid")
         sys.exit()
-    
+
     email_list_path = f"./email_lists/{domain}_emails.txt"
     os.makedirs(os.path.dirname(email_list_path), exist_ok=True)
 
     # Filter out emails from the json list
     os.system(f"jq -r '.data.emails[].value' ./json_files/{domain}_details.json > {email_list_path}")
-    
+
     email_spoofer(domain)
-   
-    
+
+
 main()
 
 
